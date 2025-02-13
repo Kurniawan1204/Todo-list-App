39608c9be772bf658c8ac71c336c514e0a43bf80
krniawn <156290585+Kurniawan1204@users.noreply.github.com>
2025-02-13 22:12:33 +0700

    Menambahkan toggle darkmode

--- public/js/script.js
+++ public/js/script.js
@@ -9,5 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
         const taskId = btn.getAttribute("data-list");
         document.getElementById("taskListId").value = taskId;
     });
-
 });
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
