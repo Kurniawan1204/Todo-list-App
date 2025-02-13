046c9695501c79b30cfc0fc973d458911a5bbbcd
krniawn <156290585+Kurniawan1204@users.noreply.github.com>
2025-02-13 14:52:14 +0700

    mengubah code navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,8 @@
-<nav class="navbar navbar-expand-lg bg-primary navbar-dark fixed-top">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background: rgb(0, 135, 197)">
     <div class="container d-flex justify-content-center">
-        <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
+        <a class="navbar-brand fw-bolder" href="#"> 
+            <i class="bi bi-stars text-warning"></i>
+            Todo List App
+        </a>
     </div>
 </nav>
